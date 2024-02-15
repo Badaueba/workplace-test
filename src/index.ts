c4f12c6a957b20fe7594cb66006d99279d46a208
Alan Alves <alan.demit@hotmail.com>
2024-02-15 14:45:36 -0300

    feat: adding public dir

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
+import { join } from "path";
+
 console.log("Thanks for using TPG!");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.get("/", (_req, res) => {
-    res.send("Hello World!");
+
+const router = express.Router();
+
+router.get("/", (_req, res, next) => {
+    const options = {
+        root: join("public"),
+    };
+
+    return res.sendFile("index.html", options, (err) => {
+        if (err) {
+            next(err);
+        }
+    });
+});
+
+app.get("/check", (_req, res) => {
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
